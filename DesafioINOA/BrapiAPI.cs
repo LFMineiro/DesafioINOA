442aefce9c1338cab188612561cd390bb54ed8f0
LFMineiro <lflmalbuquerque3@gmail.com>
2025-01-25 21:47:57 -0300

    Adicionar arquivos de projeto.

---
+++ DesafioINOA/BrapiAPI.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Globalization;
+using System.Net.Http;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace DesafioINOA
+{
+    public class Stock
+    {
+        public decimal Price { get;  }
+        public Stock(decimal price)
+        {
+           Price = price;
+        }
+    }
+
+    public class BrapiAPI
+    {
+        private const string token = "fRWFrtT59zdC6i33ekJbEL";
+        
+        public async Task<Stock> GetPrice(string ticker)
+        {
+            Stock stock;
+            decimal price = 0.0M;
+
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    string requestUrl = requestUrl = $"https://brapi.dev/api/quote/{ticker}?token={token}";
+
+                    HttpResponseMessage response = await httpClient.GetAsync(requestUrl);
+                    try
+                    {
+                        response.EnsureSuccessStatusCode();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error Requesting API:\n" + ex.Message);
+                    }
+
+                    string responseContent = await response.Content.ReadAsStringAsync();
+
+                    var json = JsonNode.Parse(responseContent)!.AsObject();
+
+                    var priceString = json["results"]?[0]?["regularMarketPrice"]?.ToString();
+                    price = Convert.ToDecimal(priceString, new CultureInfo("en-US"));
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            stock = new Stock(price);
+
+            return stock;
+
+        }
+        }
+
+    }
+
---
+++ DesafioINOA/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DesafioINOA;
+
+namespace DesafioInoaAlert
+{
+    internal static class Program
+    {
+        static async Task Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("informe o ticker do ativo como argumento.");
+                return;
+            }
+
+            string ticker = args[0];
+
+            BrapiAPI api = new BrapiAPI();
+            Stock stock = await api.GetPrice(ticker);
+
+            if (stock.Price > 0)
+            {
+                Console.WriteLine($"Preço atual do ativo {ticker}: {stock.Price}");
+            }
+            else
+            {
+                Console.WriteLine("Não foi possível obter o preço do ativo.");
+            }
+        }
+    }
+}
+
